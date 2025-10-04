3446079406b362a706f4c3c92075de494a0adc58
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2025-10-04 15:05:49 +0500

    add deleteMessage route

--- routes/message.route.js
+++ routes/message.route.js
@@ -6,9 +6,11 @@ const { isAuth } = require("../middlewares/auth.middleware");
 const {
   getMessages,
   sendMessage,
+  deleteMessage,
 } = require("../controllers/message.controller");
 
 messageRoutes.get("/messages/:id", isAuth, getMessages);
 messageRoutes.post("/send/:id", isAuth, sendMessage);
+messageRoutes.delete("/delete/:id", isAuth, deleteMessage);
 
 module.exports = messageRoutes;
