d5dce3555e4a3979d146d7904e1e0678ac97b40c
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2025-10-18 18:26:55 +0500

    add the route for the dismiss reported message

--- routes/message.route.js
+++ routes/message.route.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const messageRoutes = express.Router();
 
-const { isAuth } = require("../middlewares/auth.middleware");
+const { isAuth, isGroupAdmin } = require("../middlewares/auth.middleware");
 
 const {
   getMessages,
   sendMessage,
   deleteMessage,
   reportMessage,
+  dismissReportedMessage,
 } = require("../controllers/message.controller");
 
 messageRoutes.get("/messages/:id", isAuth, getMessages);
 messageRoutes.post("/send/:id", isAuth, sendMessage);
 messageRoutes.delete("/delete/:groupId/:messageId", isAuth, deleteMessage);
 messageRoutes.put("/report/:groupId/:messageId", isAuth, reportMessage);
+messageRoutes.put(
+  "/dsimiss/:groupId/:messageId",
+  isAuth,
+  isGroupAdmin,
+  dismissReportedMessage
+);
 
 module.exports = messageRoutes;
