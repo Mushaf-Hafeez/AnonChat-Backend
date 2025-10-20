ba81e325f63c7c0f3ba27a8f3993f87cb1e9167b
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2025-10-20 12:12:50 +0500

    fix: send message socket event argument

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -65,9 +65,7 @@ exports.sendMessage = async (req, res) => {
     delete message.sender.password;
 
     // send the message to all the groupMember
-    io.to(group).emit("new-message", {
-      message,
-    });
+    io.to(group).emit("new-message", message);
 
     // return the success response
     return res.status(200).json({
